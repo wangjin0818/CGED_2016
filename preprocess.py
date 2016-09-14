01af6c17e125d1b1c1bb5fc22cbf80e3e3cf515f
wangjin0818 <wangjin0818@gmail.com>
2016-09-14 12:10:29 +0800

    add indentification serialization

--- preprocess.py
+++ preprocess.py
@@ -11,6 +11,8 @@ import xml.dom.minidom
 
 import jieba
 
+from collections import defaultdict
+
 error_dict = {
     'R': 1,
     'M': 2,
@@ -135,6 +137,58 @@ def detect_single_serialize(file_name):
 
     return ret_id, ret_text, ret_label
 
+def identification_serialize(file_name): 
+    logging.info('Loading data from %s' % (file_name))
+
+    with codecs.open(file_name, 'r') as my_file:
+        DOMTree = xml.dom.minidom.parse(my_file)
+
+    docs = DOMTree.documentElement.getElementsByTagName('DOC')
+
+    ret_id, ret_text = [], []
+    R_label, M_label, S_label, W_label = [], [], [], []
+    for doc in docs:
+        text = doc.getElementsByTagName('TEXT')[0].childNodes[0].nodeValue.replace('\n', '')
+        text = list(jieba.cut(text, cut_all=False))
+        ret_text.append(text)
+        # ret_label.append(0)
+
+        text_id = doc.getElementsByTagName('TEXT')[0].getAttribute('id')
+
+        correction = doc.getElementsByTagName('CORRECTION')[0].childNodes[0].nodeValue.replace('\n', '')
+        correction = list(jieba.cut(correction, cut_all=False))
+        ret_text.append(correction)
+        # ret_label.append(1)
+
+        errs = doc.getElementsByTagName('ERROR')
+        cur_err_dict = defaultdict(int)
+        for err in errs:
+            err_type = err.getAttribute('type')
+            cur_err_dict[err_type] = cur_err_dict[err_type] + 1
+
+        if cur_err_dict['R'] >= 1:
+            R_label.append(1)
+        else:
+            R_label.append(0)
+
+        if cur_err_dict['M'] >= 1:
+            M_label.append(1)
+        else:
+            M_label.append(0)
+
+        if cur_err_dict['S'] >= 1:
+            S_label.append(1)
+        else:
+            S_label.append(0)
+
+        if cur_err_dict['W'] >= 1:
+            W_label.append(1)
+        else:
+            W_label.append(0)
+
+    return ret_id, ret_text, (R_label, M_label, S_label, W_label)
+
 if __name__ == '__main__':
     data = os.path.join('data', 'CGED16_HSK_Train_All.txt')
-    ret_id, ret_text, ret_label = detect_single_serialize(data)
+    # ret_id, ret_text, ret_label = detect_single_serialize(data)
+    ret_id, ret_text, label_list = identification_serialize(data)
